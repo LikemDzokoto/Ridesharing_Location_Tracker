b32a6a5cf1b2622bf4bfb48c8f1e07565067c753
Likem <likem@localhost.localdomain>
2023-01-27 02:29:59 +0000

    chore: handlejwt

--- routes /user/middleware.js
+++ routes /user/middleware.js
@@ -1,20 +1,19 @@
-const db  = require("../../models");
+const db = require("../../models");
 
-const userMiddleware = {
-    userExists : async(req,res,next) => {
-        const {
+const  userMiddleware ={
+    userExists: async(req, res,next)=>{
+         const {
             params: {id},
-        } = req;
-        const user = await db.findbyPk(id);
-        if(!user)
-        return res.status(400).send({
-            success: true,
-            message: "user not found",
-            user,
-        })
-        req.oldUser = user;
-        return next();
-    },
-};
-
-module.exports = {userMiddleware}
+         }=req;
+         const user = await db.User.findByPk(id);
+         if(!user)
+            return res.status(404).send({
+                success:true,
+                message:"user not found",
+                user,
+            });
+            req.oldUser = user;
+            return next();
+    }
+}
+module.exports   = {userMiddleware};
---
+++ utils/handleJwt.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+const handleJwt = {
+    signToken: (data) =>{
+        try{
+            const token = jwt.sign(data, process.env.JWT_SECRET_KEY);
+            return token;
+        }catch(error){
+            console.log({error});
+            throw new Error("There was a problem verifying your token",error);    
+        }
+    },
+    verifyToken:(req,res,next) => {
+        try{
+            const BearerToken = req.headers.authorization;
+            const token = BearerToken.split("")[1];
+            console.log({token});
+            const user = jwt.verify(token,process.env.JWT_SECRET_KEY);
+            req.user = user;
+            return next();
+        }catch{
+            res.status(500).send({
+                succss: false,
+                message:"invalid token, try again",
+                error,
+            });
+        } 
+
+        }
+    };
+    module.exports = {handleJwt};
